4bebd34beecbbfa7df146311fa129cc93d8ce2ee
tanveer <mdtanviralamht731@gmail.com>
2022-02-21 14:37:47 +0600

    template file added

---
+++ template.js
@@ -0,0 +1,9 @@
+const myDetails = {
+    name: 'Tanvir Alam',
+    age: 21,
+    height: 5.6,
+    weight: '55kg'
+}
+const myProfession = 'full stack developer';
+const myOutput = `Hi! I am ${myDetails.name} and my profession ${myProfession}`;
+console.log(myOutput);
