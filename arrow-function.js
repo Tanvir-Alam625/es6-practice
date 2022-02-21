636695224bfc335a942b8dd9dcc0a5346ee5a682
tanveer <mdtanviralamht731@gmail.com>
2022-02-21 14:52:48 +0600

    signle line multiply added

--- arrow-function.js
+++ arrow-function.js
@@ -1,2 +1,6 @@
+// one parameter and dividing 
 const numberDividing = (number) => number /5;
-console.log(numberDividing(50));
+// console.log(numberDividing(50));
+// tow parameters and add the tow and multiply the tow parameter
+const multiplyAddition = (fistNumber, secondNumber) =>(fistNumber + 2) * (secondNumber +2);
+console.log(multiplyAddition(10,5));  
